917de60967f38bba20a9986c93647eb653bb4bdc
强  苏 <suqiang@suqiang.local>
2022-02-13 22:14:19 +0800

    opt

--- main.py
+++ main.py
@@ -10,34 +10,33 @@ import tensorflow.keras as keras
 from tensorflow.keras import layers,Model
 
 BATCH_SIZE = 256
-value_ranges = list(zip(range(0,12),[2000] * 10)) + [(12, 300)]
+value_ranges = list(zip(range(0,16),[2000] * 16)) + [(16, 720)]
+#print(value_ranges)
 
 def make_parse_csv():
-    def parse_csv(lines):
-        fields_defaults = [int()] * 13 + [float()] * 13
-        fields = tf.io.decode_csv(lines,fields_defaults)
-
+    def parse_csv(line):
+        fields_defaults = [int()] * 17 + [float()] * 17
+        fields = tf.io.decode_csv(line,fields_defaults)
         id_features = []
         for (index, _) in value_ranges:
-            id_features.append(fields[index])
+            id_features.append(tf.clip_by_value(fields[index], clip_value_min = 0, clip_value_max = value_ranges[index][1] - 1))
         
-        float_features = [fields[i] for i in range(13, 25)]
-        numeric = tf.stack(float_features,axis=1)
-        features = tuple(id_features + numeric)
-        labels = tf.clip_by_value(fields[25],clip_value_min = 0, clip_value_max = 1)
+        float_features = [fields[i] for i in range(17, 33)]
+        features = tuple(id_features + float_features)
+        labels = tf.clip_by_value(fields[33]/720.0,clip_value_min = 0, clip_value_max = 1.0)
         return features,labels
     return parse_csv
 
 def make_dataset(filelist, repeat_times=1):
-    dataset = tf.data.TextLineDataset(filelist)
+    dataset = tf.data.TextLineDataset(filelist).skip(1)
     dataset = dataset.map(make_parse_csv())
     dataset = dataset.batch(BATCH_SIZE)
     dataset = dataset.prefetch(2 * BATCH_SIZE + 1)
     dataset = dataset.repeat(repeat_times)
     return dataset
 
-train_files = ['./data/train_0.csv']
-eval_files = ['./data/test.csv']
+train_files = ['./train_2.csv']
+eval_files = ['./test.csv']
 
 def D2Q():
     inputs=[]
@@ -48,31 +47,34 @@ def D2Q():
         x = layers.Input(shape=(1,),dtype=tf.int32)
         e = layers.Embedding(input_dim=dim, output_dim=32)(x)
         inputs.append(x)
-        pxtr_sparse_layers.append(e)
+        pxtr_sparse_concat.append(e)
+    print("pxtr_sparse_concat=", tf.shape(pxtr_sparse_concat))
     
     duration_concat = []
     x = layers.Input(shape=(1,),dtype=tf.int32)
-    e = layers.Embedding(input_dim=dim, output_dim=32)(x)
+    e = layers.Embedding(input_dim=720, output_dim=32)(x)
     inputs.append(x)
     duration_concat.append(e)
+    print("duration_concat=", tf.shape(duration_concat))
     
     pxtr_dense_concat = []
-    numeric = layers.Input(shape=(1,12),dtype=tf.float32)
-    inputs.append(numeric)
-    pxtr_dense_concat.append(numeric)
+    for i in range(16):
+        x = layers.Input(shape=(1,),dtype=tf.float32)
+        inputs.append(x)
+        pxtr_dense_concat.append(x)
     
-    wt_perc = layers.Input(shape=(1,),dtype=tf.float32)
-    inputs.append(wt_perc)
 
     hidden_layer = []
-    pxtr_sparse_emb = tf.concat(pxtr_sparse_concat,1)
+    pxtr_sparse_emb = tf.concat(pxtr_sparse_concat,2)
     for dim in [512]:
         pxtr_sparse_emb = layers.Dense(dim,tf.nn.swish)(pxtr_sparse_emb)
+    pxtr_sparse_emb = tf.reshape(pxtr_sparse_emb, [-1, 512])
     hidden_layer.append(pxtr_sparse_emb)
     
-    duration_emb = tf.concat(duration_concat,1)
+    duration_emb = tf.concat(duration_concat,2)
     for dim in [32]:
-        pxtr_sparse_emb = layers.Dense(dim,tf.nn.swish)(duration_emb)
+        duration_emb = layers.Dense(dim,tf.nn.swish)(duration_emb)
+    duration_emb = tf.reshape(duration_emb, [-1, 32])
     hidden_layer.append(duration_emb)
     
     pxtr_dense_emb = tf.concat(pxtr_dense_concat,1)
@@ -88,14 +90,16 @@ def D2Q():
 
     return Model(inputs = inputs, outputs = out)
 
+def mse_loss(labels, output):
+    loss = tf.losses.mean_squared_error(labels, output)
+    return loss
+
 def train():
     model = D2Q()
-    loss = tf.losses.mean_squared_error(labels=labels, predictions=output)
-    model.compile(loss=loss, optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.1),)
+    model.compile(loss=mse_loss, optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.1),)
     model.fit(make_dataset(train_files,1))
-    model.save_weights('./dumps/model/d2q_model')
-    
+    model.save_weights('./model/d2q_model')
+ 
 if __name__ == "__main__":
     if sys.argv[1] == 'train':
         train()
-
